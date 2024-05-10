d68048756685edc792e52d2f3ab96a2168cf2849
Nguyễn Hữu Tài <138273682+Just1nHuu@users.noreply.github.com>
2024-05-11 00:37:11 +0700

    Reset lại game
    Chỉnh sửa lại các UI có các nút quay trở về màn hình chính sẽ reset lại để chơi lại ở các bảng Over và Pause

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private Button playAgainButton;
+
     private void Start()
     {
         SuperTanksGameManager.Instance.OnStageChanged += SuperTanksGameManager_OnStateChanged;
         Hide();
     }
 
-    private void Update()
+    private void Awake()
     {
-
+        playAgainButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.Shutdown();
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
     }
     private void SuperTanksGameManager_OnStateChanged(object sender, System.EventArgs e)
     {
